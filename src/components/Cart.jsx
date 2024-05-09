3856d34463bf19edd07de6ba36d0b8927e017739
Vinit Gite <69915723+Vinit7589@users.noreply.github.com>
2024-05-09 16:18:08 +0530

    first commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 import { IoMdClose } from "react-icons/io";
-import ItemCart from './ItemCart';
+import ItemCart from "./ItemCart";
 
 const Cart = () => {
   return (
-    
-    <div className='fixed top-0 right-0 lg:w-[20vw] md:w-[40vw] w-full h-full bg-white p-4 '>
-      <div className='flex items-center justify-between my-3'>
-        <span className='text-xl font-bold text-gray-800'>My Order</span>
-        <IoMdClose className='p-0.5 text-2xl font-bold text-gray-600 border-2 border-gray-600 rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer'/>
+    <div className="fixed top-0 right-0 lg:w-[20vw] md:w-[40vw] w-full h-full bg-white p-4 ">
+      <div className="flex items-center justify-between my-3">
+        <span className="text-xl font-bold text-gray-800">My Order</span>
+        <IoMdClose className="p-0.5 text-2xl font-bold text-gray-600 border-2 border-gray-600 rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer" />
       </div>
 
       <ItemCart />
@@ -16,14 +15,16 @@ const Cart = () => {
       <ItemCart />
       <ItemCart />
 
-      <div className='absolute bottom-0 '>
-        <h3 className='font-semibold text-gray-800'>Items : </h3>
-        <h3 className='font-semibold text-gray-800'>Total Amount : </h3>
-        <hr className='lg:w-[18vw] md:w-[37vw] sm:w-[95vw] w-[90vw] my-3'/>
-        <button className='lg:w-[18vw] md:w-[37vw] sm:w-[95vw] w-[90vw] mb-4 px-2 py-1 text-white bg-green-500 rounded hover:shadow-md hover:shadow-green-400'>Checkout</button>
+      <div className="absolute bottom-0 ">
+        <h3 className="font-semibold text-gray-800">Items : </h3>
+        <h3 className="font-semibold text-gray-800">Total Amount : </h3>
+        <hr className="lg:w-[18vw] md:w-[37vw] sm:w-[95vw] w-[90vw] my-3" />
+        <button className="lg:w-[18vw] md:w-[37vw] sm:w-[95vw] w-[90vw] mb-4 px-2 py-1 text-white bg-green-500 rounded hover:shadow-md hover:shadow-green-400">
+          Checkout
+        </button>
       </div>
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 
 const ItemCart = () => {
-    return (
-        <div className='flex gap-2 p-2 mb-3 rounded shadow-md'>
-            <MdDelete />
-            <img src="https://w7.pngwing.com/pngs/21/55/png-transparent-pepperoni-pizza-margarita-pizza-margherita-italian-cuisine-tomato-pizza-food-cheese-recipe-thumbnail.png" alt="" className='w-[50px] h-[50px]' />
+  return (
+    <div className="flex gap-2 p-2 mb-3 rounded shadow-md">
+      <MdDelete
+        className="absolute right-7 hover:cursor-pointer"
+        title="Remove from the cart"
+      />
+      <img
+        src="https://w7.pngwing.com/pngs/21/55/png-transparent-pepperoni-pizza-margarita-pizza-margherita-italian-cuisine-tomato-pizza-food-cheese-recipe-thumbnail.png"
+        alt=""
+        className="w-[50px] h-[50px]"
+      />
 
-            <div className=''>
-                <h2 className='font-bold text-gray-600'>Onion Pizza</h2>
-                <div className='flex items-center'>
-                    <span className='text-green-500'>₹ 120</span>
-                    <div className='absolute flex gap-2 right-7'>
-                        <AiOutlinePlus className='p-1 text-xl text-gray-600 transition-all duration-75 ease-in-out border-2 border-gray-600 rounded cursor-pointer hover:text-white hover:bg-green-500 hover:border-none' />
-                        <span>1</span>
-                        <AiOutlineMinus className='p-1 text-xl text-gray-600 transition-all duration-75 ease-in-out border-2 border-gray-600 rounded cursor-pointer hover:text-white hover:bg-green-500 hover:border-none' />
-                    </div>
-                </div>
-            </div>
+      <div className="">
+        <h2 className="font-bold text-gray-600">Onion Pizza</h2>
+        <div className="flex items-center">
+          <span className="text-green-500">₹ 120</span>
+          <div className="absolute flex gap-2 right-7">
+            <AiOutlinePlus className="p-1 text-xl text-gray-600 transition-all duration-75 ease-in-out border-2 border-gray-600 rounded cursor-pointer hover:text-white hover:bg-green-500 hover:border-none" />
+            <span>1</span>
+            <AiOutlineMinus className="p-1 text-xl text-gray-600 transition-all duration-75 ease-in-out border-2 border-gray-600 rounded cursor-pointer hover:text-white hover:bg-green-500 hover:border-none" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ItemCart
+export default ItemCart;
